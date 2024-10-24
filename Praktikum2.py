2bc6fb4d96039d01fb79a282beacf4b96fbd5e79
vivitnh23 <vivitnh1@gmail.com>
2024-10-24 15:55:23 +0700

    Add files via upload

---
+++ Praktikum2.py
@@ -0,0 +1,10 @@
+max = 0
+
+bilangan = int(input("masukan bilangan: "))
+while bilangan != 0:
+    if bilangan > max :
+        max = bilangan 
+    bilangan = int(input("masukan bilangan:"))
+
+print(f"bilangan terbesar= {max}")
+
